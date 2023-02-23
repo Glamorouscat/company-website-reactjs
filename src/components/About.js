5260e1bcfbfbf644fd7ff032d53b830f0566209e
Glamorouscat <glamorouscat@163.com>
2023-02-23 14:31:48 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -11,20 +11,13 @@ function About() {
           <div className="col-md-6">
             <h2 className="main-title about-h2">ABOUT</h2>
             <p className="main-p">
-              At Wood Bliss, we believe that every home deserves the warmth and beauty of 
-              
+                  At Wood Bliss, we believe that every home deserves the warmth and beauty of 
               natural wood products. That's why we carefully select only the finest solid woods and 
-
               craft them into timeless pieces that will last a lifetime. We understand the 
-              
               importance of sustainable practices, and so we work closely with our suppliers to 
-              
               ensure that our materials are responsibly sourced. From the moment you hold one of 
-              
               our pieces, you'll feel the quality and care that goes into each and every one. We're 
-              
               passionate about bringing the natural beauty of wood into your home, and we hope 
-              
               that our products will inspire you to create your own little piece of Wood Bliss.
             </p>
           </div>
