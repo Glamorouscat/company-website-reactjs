f40e753d31193e6aa2c3622216b95a1669fe5620
Glamorouscat <glamorouscat@163.com>
2023-02-23 12:51:41 +0800

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,13 +7,13 @@ function Services() {
             <div className="col-md-12">
                 <div className="row">
                     <div className="col-md-4 mb-2">
-                        <Card title="Web Development" img="card1.png" text="Morbi eget neque risus. Duis erat quam, porta quis enim id, venenatis blandit nunc. " />
+                        <Card title="Customization" img="card1.png" text="At Wood Bliss, we understand that every home is unique, and we believe that your furniture should be too. That's why we offer a customization service, where you can work with our skilled craftsmen to design and create a piece that perfectly fits your space and style. Whether it's a one-of-a-kind coffee table or a custom bookshelf, we're committed to bringing your vision to life. " />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="E-Commerce Services" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
+                    <Card title="Care and maintenance" img="card2.png" text="We're not just passionate about creating beautiful solid wood products; we're also committed to ensuring that they last a lifetime. That's why we offer care and maintenance services, including regular cleaning and conditioning, to help preserve the natural beauty of your furniture for years to come. We're always available to answer any questions you may have about how to care for your Wood Bliss pieces. " />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="Cyber Security" img="card3.png" text="Phasellus suscipit nibh at nisi finibus vestibulum sit amet vitae massa. Suspendisse non est eget elit pulvinar consectetur nec non sapien." />
+                    <Card title="Eco-friendly practices" img="card3.png" text="Sustainability is at the heart of everything we do at Wood Bliss. We believe that it's our responsibility to protect the environment and ensure that future generations can enjoy the natural beauty of wood just as we do today. Thatwork's why we With responsible suppliers who use sustainable forestry practices, and we use eco-friendly finishes and materials whenever possible. When you choose Wood Bliss, you can feel good knowing that you're supporting a company that cares about the planet. " />
                     </div>
                 </div>
             </div>
